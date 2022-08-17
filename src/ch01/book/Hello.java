8379962323ff4b83bf18d95cab795d2b9ca45860
seosum <110441721+seosum@users.noreply.github.com>
2022-08-17 12:13:35 +0900

    first commit

---
+++ src/ch01/book/Hello.java
@@ -0,0 +1,11 @@
+package ch01.book;
+/*
+ * 작성자 : 신용권
+ * 작성일 : 2015.12.31
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+	}
+}
---
+++ src/ch01/book/RunStatementExample.java
@@ -0,0 +1,10 @@
+package ch01.book;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(x + "+" + y + "=" + result);
+	}
+}
