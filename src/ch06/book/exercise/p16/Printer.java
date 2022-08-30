30f2b27dd9b0e0cb66be5f9619ad552526bad156
seosum <110441721+seosum@users.noreply.github.com>
2022-08-30 09:12:58 +0900

    20220829

---
+++ src/ch06/book/exercise/p16/Printer.java
@@ -0,0 +1,19 @@
+package ch06.book.exercise.p16;
+
+public class Printer {
+	void println(int i) {
+		System.out.println(i);
+	}
+
+	public void println(boolean b) {
+		System.out.println(b);
+	}
+
+	public void println(double d) {
+		System.out.println(d);
+	}
+
+	public void println(String string) {
+		System.out.println(string);
+	}
+}
