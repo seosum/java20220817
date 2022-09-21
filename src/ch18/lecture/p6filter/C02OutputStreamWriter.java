f2981821d236d7abbe34bc01d2c6f728034ddb45
seosum <ssm347574248@gmail.com>
2022-09-21 19:08:39 +0900

    20220921 io

--- src/ch18/lecture/p6filter/C02OutputStreamWriter.java
+++ src/ch18/lecture/p6filter/C02OutputStreamWriter.java
@@ -9,17 +9,26 @@ public class C02OutputStreamWriter {
 	public static void main(String[] args) throws Exception{
 
 		String name = "C:\\Users\\user\\Desktop\\output\\output9.txt";
-		
-		FileOutputStream fos = getFileOutPutStream(name);
+		FileOutputStream fos = getFileOutputStream(name);
 		Writer wr = new OutputStreamWriter(fos);
 		
-		fos.write('a');
-		fos.write('b');
-		fos.write('가');
-		fos.write('나');
+//		fos.write('a');
+//		fos.write('b');
+//		fos.write('가');
+//		fos.write('나');
+		
+		wr.write('a');
+		wr.write('b');
+		wr.write('가');
+		wr.write('나');
+		
 		
 		wr.close();
 		fos.close();
+		
 	}
-
+	
+	private static FileOutputStream getFileOutputStream(String fileName) throws Exception {
+		return new FileOutputStream(fileName);
+	}
 }
